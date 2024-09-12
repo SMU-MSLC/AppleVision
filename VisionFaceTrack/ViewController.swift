79377cbb33c8bedfdead431bdde1d464604ab723
Eric Larson <eclarson@smu.edu>
2024-09-12 17:28:50 -0500

    tracking almost done

--- VisionFaceTrack/ViewController.swift
+++ VisionFaceTrack/ViewController.swift
@@ -24,6 +24,11 @@ class ViewController: UIViewController {
     var captureDevice: AVCaptureDevice?
     var captureDeviceResolution: CGSize = CGSize()
     
+    // Layer UI for drawing Vision results
+    var rootLayer: CALayer?
+    var detectionOverlayLayer: CALayer?
+    var detectedFaceRectangleShapeLayer: CAShapeLayer?
+    var detectedFaceLandmarksShapeLayer: CAShapeLayer?
     
     // Vision requests
     private var detectionRequests: [VNDetectFaceRectanglesRequest]?
@@ -80,6 +85,9 @@ class ViewController: UIViewController {
         // setup the tracking of a sequence of features from detection
         self.sequenceRequestHandler = VNSequenceRequestHandler()
         
+        // setup bounding box display using pre-made layers
+        // this creates the layers that the face will update
+        self.setupVisionDrawingLayers()
     }
     
     // define behavior for when we detect a face
@@ -191,9 +199,15 @@ class ViewController: UIViewController {
             // if we made it here, then we do have valid tracking
             //  of the initially detected face!!
             
+            DispatchQueue.main.async{
+                self.drawFaceObservations(newTrackingRequests)
+            }
+            
             if let singleTrack = newTrackingRequests.last{
                 //TODO: make this update a single bounding box layer
                 print(singleTrack.inputObservation.boundingBox.origin)
+                
+                
             }
             
         }
@@ -430,6 +444,7 @@ extension ViewController:AVCaptureVideoDataOutputSampleBufferDelegate{
         videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
         
         if let previewRootLayer = self.previewView?.layer {
+            self.rootLayer = previewRootLayer
             
             previewRootLayer.masksToBounds = true
             videoPreviewLayer.frame = previewRootLayer.bounds
@@ -449,5 +464,113 @@ extension ViewController:AVCaptureVideoDataOutputSampleBufferDelegate{
     }
 }
 
-
+// MARK: Extension Drawing Vision Observations
+extension ViewController {
+    
+    
+    fileprivate func setupVisionDrawingLayers() {
+        let captureDeviceResolution = self.captureDeviceResolution
+        
+        let captureDeviceBounds = CGRect(x: 0,
+                                         y: 0,
+                                         width: captureDeviceResolution.width,
+                                         height: captureDeviceResolution.height)
+        
+        let captureDeviceBoundsCenterPoint = CGPoint(x: captureDeviceBounds.midX,
+                                                     y: captureDeviceBounds.midY)
+        
+        let normalizedCenterPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        guard let rootLayer = self.rootLayer else {
+            self.presentErrorAlert(message: "view was not property initialized")
+            return
+        }
+        
+        let overlayLayer = CALayer()
+        overlayLayer.name = "DetectionOverlay"
+        overlayLayer.masksToBounds = true
+        overlayLayer.anchorPoint = normalizedCenterPoint
+        overlayLayer.bounds = captureDeviceBounds
+        overlayLayer.position = CGPoint(x: rootLayer.bounds.midX, y: rootLayer.bounds.midY)
+        
+        let faceRectangleShapeLayer = CAShapeLayer()
+        faceRectangleShapeLayer.name = "RectangleOutlineLayer"
+        faceRectangleShapeLayer.bounds = captureDeviceBounds
+        faceRectangleShapeLayer.anchorPoint = normalizedCenterPoint
+        faceRectangleShapeLayer.position = captureDeviceBoundsCenterPoint
+        faceRectangleShapeLayer.fillColor = nil
+        faceRectangleShapeLayer.strokeColor = UIColor.green.withAlphaComponent(0.7).cgColor
+        faceRectangleShapeLayer.lineWidth = 5
+        faceRectangleShapeLayer.shadowOpacity = 0.7
+        faceRectangleShapeLayer.shadowRadius = 5
+        
+        
+        
+        overlayLayer.addSublayer(faceRectangleShapeLayer)
+        rootLayer.addSublayer(overlayLayer)
+        
+        self.detectionOverlayLayer = overlayLayer
+        self.detectedFaceRectangleShapeLayer = faceRectangleShapeLayer
+        
+        self.updateLayerGeometry()
+    }
+    
+    fileprivate func updateLayerGeometry() {
+        guard let overlayLayer = self.detectionOverlayLayer,
+            let rootLayer = self.rootLayer,
+            let previewLayer = self.previewLayer
+            else {
+            return
+        }
+        
+        CATransaction.setValue(NSNumber(value: true), forKey: kCATransactionDisableActions)
+        
+        let videoPreviewRect = previewLayer.layerRectConverted(fromMetadataOutputRect: CGRect(x: 0, y: 0, width: 1, height: 1))
+        
+        var rotation: CGFloat
+        var scaleX: CGFloat
+        var scaleY: CGFloat
+        
+        // Rotate the layer into portrait orientation.
+        rotation = 0
+        scaleX = videoPreviewRect.width / captureDeviceResolution.width
+        scaleY = videoPreviewRect.height / captureDeviceResolution.height
+        
+        // Scale and mirror the image to ensure upright presentation.
+        let affineTransform = CGAffineTransform(rotationAngle: radiansForDegrees(rotation))
+            .scaledBy(x: scaleX, y: -scaleY)
+        overlayLayer.setAffineTransform(affineTransform)
+        
+        // Cover entire screen UI.
+        let rootLayerBounds = rootLayer.bounds
+        overlayLayer.position = CGPoint(x: rootLayerBounds.midX, y: rootLayerBounds.midY)
+    }
+    
+    
+    /// - Tag: DrawPaths
+    fileprivate func drawFaceObservations(_ faceObservations: [VNFaceObservation]) {
+        guard let faceRectangleShapeLayer = self.detectedFaceRectangleShapeLayer
+            else {
+            return
+        }
+        
+        CATransaction.begin()
+        
+        CATransaction.setValue(NSNumber(value: true), forKey: kCATransactionDisableActions)
+        
+        let faceRectanglePath = CGMutablePath()
+        let displaySize = self.captureDeviceResolution
+        
+        for faceObservation in faceObservations {
+            let faceBounds = VNImageRectForNormalizedRect(faceObservation.boundingBox, Int(displaySize.width), Int(displaySize.height))
+            faceRectanglePath.addRect(faceBounds)
+        }
+        
+        faceRectangleShapeLayer.path = faceRectanglePath
+        
+        self.updateLayerGeometry()
+        
+        CATransaction.commit()
+    }
+}
 
